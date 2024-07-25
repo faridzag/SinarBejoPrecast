31a104898f11056d2ee7939dff9af3b155256d9e
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-07-25 21:13:43 +0700

    deleteing fav icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-import htmx from 'astro-htmx';
+import htmx from "astro-htmx";
 import netlify from "@astrojs/netlify";
 
 import vercel from "@astrojs/vercel/serverless";
@@ -11,30 +11,34 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://sinarbejoprecast.com",
+  site: "https://sinarbejoprecast.vercel.app",
   // image: {
   //   domains: ["images.unsplash.com"]
   // },
   prefetch: true,
-  integrations: [htmx(), tailwind(), sitemap({
-    i18n: {
-      defaultLocale: "id",
-      // All urls that don't contain `fr` after `https://sinarbejoprecast.com/` will be treated as default locale, i.e. `en`
-      locales: {
-        id: "id" // The `defaultLocale` value must present in `locales` keys
-      }
-    }
-  }), compressor({
-    gzip: false,
-    brotli: true
-  })],
+  integrations: [
+    htmx(),
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "id",
+        // All urls that don't contain `fr` after `https://sinarbejoprecast.com/` will be treated as default locale, i.e. `en`
+        locales: {
+          id: "id", // The `defaultLocale` value must present in `locales` keys
+        },
+      },
+    }),
+    compressor({
+      gzip: false,
+      brotli: true,
+    }),
+  ],
   output: "server",
   experimental: {
     clientPrerender: true,
-    directRenderScript: true
-  }
+    directRenderScript: true,
+  },
   // adapter: vercelStatic(),
-  ,
-
-  adapter: vercel()
+  adapter: vercel(),
 });
+
