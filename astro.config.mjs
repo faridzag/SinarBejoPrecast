d931da60585b6edaa6d370c86392ca0815faf73f
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-07-25 11:31:27 +0700

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,35 +5,34 @@ import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import htmx from 'astro-htmx';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://sinarbejoprecast.com",
   image: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com"]
   },
-
   prefetch: true,
-  integrations: [
-    htmx(),
-    tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: "id", // All urls that don't contain `fr` after `https://sinarbejoprecast.com/` will be treated as default locale, i.e. `en`
-        locales: {
-          id: "id", // The `defaultLocale` value must present in `locales` keys
-        },
-      },
-    }),
-    compressor({
-      gzip: false,
-      brotli: true,
-    }),
-  ],
+  integrations: [htmx(), tailwind(), sitemap({
+    i18n: {
+      defaultLocale: "id",
+      // All urls that don't contain `fr` after `https://sinarbejoprecast.com/` will be treated as default locale, i.e. `en`
+      locales: {
+        id: "id" // The `defaultLocale` value must present in `locales` keys
+      }
+    }
+  }), compressor({
+    gzip: false,
+    brotli: true
+  })],
   output: "server",
   experimental: {
     clientPrerender: true,
-    directRenderScript: true,
-  },
+    directRenderScript: true
+  }
   // adapter: vercelStatic(),
+  ,
+  adapter: netlify()
 });
