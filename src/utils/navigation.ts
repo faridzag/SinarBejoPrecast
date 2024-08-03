15be4247efaa2f02a493ba6a5809392928af45d3
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-08-03 20:33:03 +0700

    add about page

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -2,28 +2,34 @@
 const navBarLinks = [
   { name: "Home", url: "/" },
   { name: "Produk", url: "/products" },
-  { name: "Layanan", url: "/services" },
   { name: "Kontak", url: "/contact" },
+  { name: "Tentang Kami", url: "/about" },
 ];
 // An array of links for footer
 const footerLinks = [
   {
-    section: "Ekosistem",
+    section: "Perusahaan",
     links: [
-      { name: "Link", url: "#" },
+      { name: "Tentang kami", url: "/about" },
       { name: "Produk", url: "/products" },
-      { name: "Layanan", url: "/services" },
     ],
   },
   {
-    section: "Perusahaan",
+    section: "Hubungi Kami",
     links: [
-      { name: "Tentang kami", url: "#" },
-      { name: "Alamat", url: "#" },
+      {
+        name: "Jl. Lidjen, Dsn. Krajan, RT/RW 001/002, Desa Licin, Kec. Licin, Kab Banyuwangi",
+      },
+      {
+        name: "Email: cv.sinarbejo@gmail.com",
+        url: "mailto:cv.sinarbejo@gmail.com",
+      },
+      { name: "Telp: 0852 3009 9980" },
     ],
   },
 ];
 // An object of links for social icons
+// TODO: Link Toko, Shopee, Tokped
 const socialLinks = {
   facebook: "https://www.facebook.com/",
   x: "https://twitter.com/",
